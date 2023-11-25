2839f592f57369b35d97cf034e6ae7b9182c276e
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-11-25 16:03:17 -0300

    commit

--- assets/js/produtos/ProdutosJs.js
+++ assets/js/produtos/ProdutosJs.js
@@ -3,31 +3,42 @@ const CONTAINER_PRODUTOS = document.querySelector("#containerProdutos");
 const SELECT_PRECO = document.querySelector("#preco");
 const SELECT_ORDEM_ALFA = document.querySelector("#ordemAlfa");
 
-function ordenarProdutos() {
+function ordenarPorPreco() {
   let produtosArray = Array.from(PRODUTOS);
-
+  SELECT_ORDEM_ALFA.selectedIndex = 0;
   produtosArray.sort((a, b) => {
+    const precoA = parseFloat(a.querySelector("#precoInteiro").textContent);
+    const precoB = parseFloat(b.querySelector("#precoInteiro").textContent);
+
     if (SELECT_PRECO.value === "maior") {
-      const precoA = parseFloat(a.querySelector("#precoInteiro").textContent);
-      const precoB = parseFloat(b.querySelector("#precoInteiro").textContent);
-      if (precoA !== precoB) {
-        return precoB - precoA;
-      }
+      return precoB - precoA;
     } else if (SELECT_PRECO.value === "menor") {
-      const precoA = parseFloat(a.querySelector("#precoInteiro").textContent);
-      const precoB = parseFloat(b.querySelector("#precoInteiro").textContent);
-      if (precoA !== precoB) {
-        return precoA - precoB;
-      }
+      return precoA - precoB;
     }
 
-    if (SELECT_ORDEM_ALFA.value === "AC") {
-      const nomeA = a.querySelector("h3").textContent.toLowerCase();
-      const nomeB = b.querySelector("h3").textContent.toLowerCase();
+    return 0; // Nenhuma ordenação específica, mantém a ordem original
+  });
+
+  // Limpar o contêiner
+  CONTAINER_PRODUTOS.innerHTML = "";
+
+  // Adicionar produtos ordenados ao contêiner
+  produtosArray.forEach((produto) => {
+    CONTAINER_PRODUTOS.appendChild(produto);
+  });
+}
+
+function ordenarPorOrdemAlfa() {
+  let produtosArray = Array.from(PRODUTOS);
+  SELECT_PRECO.selectedIndex = 0;
+
+  produtosArray.sort((a, b) => {
+    const nomeA = a.querySelector("h3").textContent.toLowerCase();
+    const nomeB = b.querySelector("h3").textContent.toLowerCase();
+
+    if (SELECT_ORDEM_ALFA.value === "AZ") {
       return nomeA.localeCompare(nomeB);
     } else if (SELECT_ORDEM_ALFA.value === "ZA") {
-      const nomeA = a.querySelector("h3").textContent.toLowerCase();
-      const nomeB = b.querySelector("h3").textContent.toLowerCase();
       return nomeB.localeCompare(nomeA);
     }
 
@@ -43,5 +54,5 @@ function ordenarProdutos() {
   });
 }
 
-SELECT_PRECO.addEventListener("change", ordenarProdutos);
-SELECT_ORDEM_ALFA.addEventListener("change", ordenarProdutos);
+SELECT_PRECO.addEventListener("change", ordenarPorPreco);
+SELECT_ORDEM_ALFA.addEventListener("change", ordenarPorOrdemAlfa);
