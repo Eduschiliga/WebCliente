af3371128a5321bcb205ebe2b273ec55dc0ccd5e
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-12-07 17:29:07 -0300

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,13 @@ if (USUARIO) {
   NOME_USER.innerHTML = `${USUARIO.nome}`;
   NOME_USER.setAttribute("style", "display: block");
 }
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // Quando a rolagem é maior que 200 na altura da janela de visualização, adicione a classe show-scroll à tag de rolagem
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
